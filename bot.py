836d74d94caedb54f40d404f55c521ab5691d2c0
Márcio Luís Günther <marcio@marciogunther.com>
2020-06-19 16:43:56 -0300

    First commit

---
+++ bot.py
@@ -0,0 +1,17 @@
+from telegram.ext import Updater, CommandHandler
+
+def welcome(update, context):
+    message = 'Olá ' + update.message.from_user.first_name + "!"
+    print(message)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+def main():
+    token = '1182770672:AAE2ejI2vsG8kWLOSbpi8le0cdGbS9f09Ms'
+    updater = Updater(token=token, use_context=True)
+    updater.dispatcher.add_handler(CommandHandler('start', welcome))
+    updater.start_polling()
+    print('Oi, eu sou o updater ' + str(updater))
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
